3e8cbe0b40d14c10a51063b3af9df7fee8da2d05
Amir Ayed <146146233+amirayed7@users.noreply.github.com>
2024-08-01 09:37:20 -0400

    easy

---
+++ Leetcode75/1431.js
@@ -0,0 +1,17 @@
+//There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+//Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let newAmount = candies.map(a=> a + extraCandies)
+
+    let max = Math.max(...candies)
+
+    return newAmount.map(i=> i >= max)
+};
