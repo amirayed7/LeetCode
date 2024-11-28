503903df272c7267a187d6fc60bf647ef9587e70
Amir Ayed <146146233+amirayed7@users.noreply.github.com>
2024-11-28 12:29:40 -0500

    [26] [Time Beats: 100.00%] [Memory Beats: 15.22%] - LeetPush

---
+++ 26-Remove-Duplicates-from-Sorted-Array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let pos = 0
+
+  for (let i = 1; i < nums.length; i++){
+    if (nums[i] !== nums[pos]){
+        pos++
+        nums[pos] = nums[i]    
+    }
+  }
+
+  return pos + 1
+};
