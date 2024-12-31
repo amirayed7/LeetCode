390dc8fd68b567a1d0bf77855349c579d108743a
Amir Ayed <146146233+amirayed7@users.noreply.github.com>
2024-12-31 07:19:25 -0500

    [88] [Time Beats: 100.00%] [Memory Beats: 29.74%] - LeetPush

--- 88-Merge-Sorted-Array.js
+++ 88-Merge-Sorted-Array.js
@@ -7,27 +7,22 @@
  */
 var merge = function(nums1, m, nums2, n) {
     let p1 = m - 1
-    let p2 = n - 1
+    let p2 = n -1
     let p3 = m + n - 1
 
-    while(p1 > -1 && p2 > -1){
-        if (nums2[p2] >= nums1[p1]){
-            nums1[p3] = nums2[p2]
-            p3--
-            p2--
+    while(p1 >= 0 && p2 >= 0){
+        if (nums1[p1] >= nums2[p2]){
+            nums1[p3--] = nums1[p1--]
         }
         else{
-            nums1[p3] = nums1[p1]
-            p3--
-            p1--
+            nums1[p3--] = nums2[p2--] 
         }
     }
 
     while (p2 >= 0){
-        nums1[p3] = nums2[p2]
-        p3--
-        p2--
+        nums1[p3--] = nums2[p2--] 
     }
 
     return nums1
 };
+
